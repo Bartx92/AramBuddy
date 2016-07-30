909870a8f688cd5cc017afdd8eaf98489e7e560c
plsfixrito <asurabro@gmail.com>
2016-07-30 15:05:25 +0300

    taliyah

--- AramBuddy/AramBuddy/GenesisSpellDatabase/Spells/Spells.cs
+++ AramBuddy/AramBuddy/GenesisSpellDatabase/Spells/Spells.cs
@@ -2100,6 +2100,25 @@ namespace GenesisSpellLibrary.Spells
         }
     }
 
+    public class Taliyah : SpellBase
+    {
+        public override sealed Spell.SpellBase Q { get; set; }
+
+        public override sealed Spell.SpellBase W { get; set; }
+
+        public override sealed Spell.SpellBase E { get; set; }
+
+        public override sealed Spell.SpellBase R { get; set; }
+
+        public Taliyah()
+        {
+            Q = new Spell.Skillshot(SpellSlot.Q, 900, SkillShotType.Linear, 250, 2000, 60) { AllowedCollisionCount = 0 };
+            W = new Spell.Skillshot(SpellSlot.W, 800, SkillShotType.Circular, 250, int.MaxValue, 180);
+            E = new Spell.Skillshot(SpellSlot.E, 700, SkillShotType.Cone);
+            this.WisCC = true;
+        }
+    }
+
     public class Talon : SpellBase
     {
         public override sealed Spell.SpellBase Q { get; set; }
